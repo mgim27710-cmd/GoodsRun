77a577542a0c148ccb2105a4acf6bbfcea3cef7f
김민규 <hamobaby@naver.com>
2026-09-12 14:53:47 +0900

    chore: Spring Boot 및 MySQL DB 연동 초기 세팅

---
+++ src/main/java/goodsrun_backend/GoodsrunBackendApplication.java
@@ -0,0 +1,13 @@
+package goodsrun_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsrunBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsrunBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goodsrun_backend/GoodsrunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package goodsrun_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsrunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
